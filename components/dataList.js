fb2a1bed270667341e8482058f8886cf4f755414
Kristian <kristian@coulsy.co.uk>
2018-10-09 22:11:45 +0100

    Refactored dataList foods from string to an array of objects

--- components/dataList.js
+++ components/dataList.js
@@ -1,39 +1,172 @@
-export default [
-  {
+export default [{
     id: 0,
     image: "/images/viral-fever.jpg",
     sick: "Viral Fever",
-    goodFoods: `Coriander Tea, Bland Diet, Gargle With Salt Water, Eat Raw Garlic &
-    Onion, "Honey, Lemon and Ginger Juice", Tulsi(Basil) Leaves`
-  },
-  {
+    goodFoods: {
+        vegan: [{
+            name: "Coriander Tea",
+            desc: ""
+        }, {
+            name: "Eat Raw Garlic & Onion",
+            desc: ""
+        }, {
+            name: "Honey, Lemon and Ginger Juice",
+            desc: ""
+        }, {
+            name: "Tulsi(Basil) Leaves",
+            desc: ""
+        }],
+        nonVegan: [{
+            name: "Bland Diet",
+            desc: ""
+        }, {
+            name: "Gargle With Salt Water",
+            desc: ""
+        }]
+    }
+}, {
     id: 1,
     image: "https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiIjYXy7_bdAhVKs48KHVTGCMEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.medicalnewstoday.com%2Farticles%2F73936.php&psig=AOvVaw0AxJ5ySRVR0Xxv9tzpTxXe&ust=1539089109264432",
     sick: "Headaches",
-    goodFoods: `Russet Potato, Cucumber, Cherries, Fortified Whole Grain Cereal, Hot Peppers, Pumpkin Seeds, Oatmeall, Brown Rice`
-  },
-  {
+    goodFoods: {
+        vegan: [{
+            name: "Russet Potato",
+            desc: ""
+        }, {
+            name: "Cucumber",
+            desc: ""
+        }, {
+            name: "Cherries",
+            desc: ""
+        }, {
+            name: "Fortified Whole Grain Cereal",
+            desc: ""
+        }, {
+            name: "Hot Peppers",
+            desc: ""
+        }, {
+            name: "Pumpkin Seeds",
+            desc: ""
+        }, {
+            name: "Oatmeall",
+            desc: ""
+        }, {
+            name: "Brown Rice",
+            desc: ""
+        }],
+        nonVegan: []
+    }
+}, {
     id: 2,
     image: "https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjV9Nqr8PbdAhUhR48KHYjvD2AQjRx6BAgBEAU&url=http%3A%2F%2Fhome.bt.com%2Flifestyle%2Fhealth%2Fhealth-concerns%2Fwhat-is-aussie-flu-11364215917573&psig=AOvVaw0d0_v2vbG3uPCYjcUWvQgp&ust=1539089234071343",
     sick: "Flu",
-    goodFoods: `Bananas, Rice, Applesauce, Toast, Flat ginger ale, Coke, Saltines`
-  },
-  {
+    goodFoods: {
+        vegan: [{
+            name: "Bananas",
+            desc: ""
+        }, {
+            name: "Rice",
+            desc: ""
+        }, {
+            name: "Applesauce",
+            desc: ""
+        }, {
+            name: "Toast",
+            desc: ""
+        }, {
+            name: "Flat ginger ale",
+            desc: ""
+        }, {
+            name: "Coke",
+            desc: ""
+        }, {
+            name: "Saltines",
+            desc: ""
+        }],
+        nonVegan: []
+    }
+}, {
     id: 3,
     image: "https://www.findatopdoc.com/var/fatd/storage/images/healthy-living/the-common-cold-symptoms-and-management/67599-1-eng-US/The-Common-Cold-Symptoms-and-Management_article_main.jpg",
     sick: "Common Cold",
-    goodFoods: `Chicken Soup, Broths, Garlic, Coconut Water, Hot Tea, Honey, Ginger, Spicy Foods, Bananas, Oatmeal, Yogurt, Avocados`
-  },
-  {
+    goodFoods: {
+        vegan: [{
+            name: "Broths",
+            desc: ""
+        }, {
+            name: "Garlic",
+            desc: ""
+        }, {
+            name: "Coconut Water",
+            desc: ""
+        }, {
+            name: "Hot Tea",
+            desc: ""
+        }, {
+            name: "Honey",
+            desc: ""
+        }, {
+            name: "Ginger",
+            desc: ""
+        }, {
+            name: "Spicy Foods",
+            desc: ""
+        }, {
+            name: "Bananas",
+            desc: ""
+        }, {
+            name: "Oatmeal",
+            desc: ""
+        }, {
+            name: "Yogurt",
+            desc: ""
+        }, {
+            name: "Avocados",
+            desc: ""
+        }],
+        nonVegan: [{
+            name: "Chicken Soup",
+            desc: ""
+        }]
+    }
+}, {
     id: 4,
     image: "http://spydersalejacket.net/wp-content/uploads/2016/01/hot-drinks-4.jpg",
     sick: "It hurts to swallow",
-    goodFoods: `Combine peppermint tea (lukewarm, not hot)—which has analgesic and anesthetic effects—and Manuka honey, which is known for its wound-healing properties. Soft, creamy foods such as cream soups, mashed potatoes, yogurt, scrambled eggs, and custards are also soothing. `
-  },
-  {
+    goodFoods: {
+        vegan: [{
+            name: "Combine peppermint tea",
+            desc: "(lukewarm, not hot) which has analgesic and anesthetic effects"
+        }, {
+            name: "Manuka honey",
+            desc: "Known for its wound-healing properties"
+        }, {
+            name: "Soft creamy foods such as cream soups",
+            desc: ""
+        }, {
+            name: "Mashed Potatoes",
+            desc: ""
+        }, {
+            name: "Yogurt",
+            desc: ""
+        }, {
+            name: "Custards",
+            desc: "Also soothing"
+        }],
+        nonVegan: [{
+            name: "Scrambled eggs",
+            desc: ""
+        }]
+    }
+}, {
     id: 5,
     image: "https://www.thespruceeats.com/thmb/hh7q_4n69WWWKpHxIAgRao_oatA=/2048x1365/filters:no_upscale():max_bytes(150000):strip_icc()/homemade-ginger-tea-3377239-5-5_preview-5afd9c53c06471003692829d.jpeg",
     sick: "You have a runny nose",
-    goodFoods: `Try Wakaya ginger tea, suggests Slayton. Ginger contains antioxidant and anti-inflammatory properties that may help clear up a cold faster than just waiting it out`
-  }
-]
+    goodFoods: {
+        vegan: [{
+            name: "Wakaya ginger tea",
+            desc: "Suggests Slayton. Ginger contains antioxidant and anti-inflammatory properties that may help clear up a cold faster than just waiting it out"
+        }],
+        nonVegan: []
+    }
+}]
