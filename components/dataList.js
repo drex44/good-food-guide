7053405b946ded048238ab30414370dcb62e414a
Harry A.M <harry.andriyan@sebangsa.com>
2018-10-09 16:20:05 +0700

    adding 2 datas : runny node and hurts to swallow

--- components/dataList.js
+++ components/dataList.js
@@ -17,5 +17,17 @@ export default [
     image: "https://www.google.co.id/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjV9Nqr8PbdAhUhR48KHYjvD2AQjRx6BAgBEAU&url=http%3A%2F%2Fhome.bt.com%2Flifestyle%2Fhealth%2Fhealth-concerns%2Fwhat-is-aussie-flu-11364215917573&psig=AOvVaw0d0_v2vbG3uPCYjcUWvQgp&ust=1539089234071343",
     sick: "Flu",
     goodFoods: `Bananas, Rice, Applesauce, Toast, Flat ginger ale, Coke, Saltines`
+  },
+  {
+    id: 3,
+    image: "http://spydersalejacket.net/wp-content/uploads/2016/01/hot-drinks-4.jpg",
+    sick: "It hurts to swallow",
+    goodFoods: `Combine peppermint tea (lukewarm, not hot)—which has analgesic and anesthetic effects—and Manuka honey, which is known for its wound-healing properties. Soft, creamy foods such as cream soups, mashed potatoes, yogurt, scrambled eggs, and custards are also soothing. `
+  },
+  {
+    id: 4,
+    image: "https://www.thespruceeats.com/thmb/hh7q_4n69WWWKpHxIAgRao_oatA=/2048x1365/filters:no_upscale():max_bytes(150000):strip_icc()/homemade-ginger-tea-3377239-5-5_preview-5afd9c53c06471003692829d.jpeg",
+    sick: "You have a runny nose",
+    goodFoods: `Try Wakaya ginger tea, suggests Slayton. Ginger contains antioxidant and anti-inflammatory properties that may help clear up a cold faster than just waiting it out `
   }
 ]
