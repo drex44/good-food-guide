d508b8433180855418a8b2c9930a350eea8a50a9
Tahirih <tahirih.jaliri@nodos.pe>
2018-10-10 12:10:58 -0500

    styles food cards

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -18,7 +18,7 @@ class FoodCard extends Component {
     return (
       <Grid container spacing={8}>
         {Data.map((data, index) => (
-          <Grid item xs={12} lg={6}>                 
+          <Grid item xs={12} md={6} lg={3} style={{display: 'flex'}}>                 
             <Card raised key={index} style={{ marginBottom: 15, width: '100%' }}>
               <CardActionArea style={{ width: '100%' }}>
                 <CardMedia style={{height: 0, paddingTop: '25%'}} image={data.image} title={data.sick} />
