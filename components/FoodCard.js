a156c8f02b2b27017bde241602229db694a8c870
Dhanraj Acharya <dhanrajacharya44@gmail.com>
2018-10-11 15:40:10 +0530

    Separated data login from FoodCard to index.js(container)

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Data from "./dataList";
 import FoodList from "./FoodList";
 import {
   Card,
@@ -13,61 +12,55 @@ import {
 } from "@material-ui/core";
 import Link from "next/link";
 
-class FoodCard extends Component {
-  render() {
-    return (
-      <Grid container spacing={8}>
-        {Data.map((data, index) => (
-          <Grid item xs={12} md={6} lg={3} style={{ display: "flex" }}>
-            <Card
-              raised
-              key={index}
-              style={{ marginBottom: 15, width: "100%" }}
-            >
-              <CardActionArea style={{ width: "100%" }}>
-                <CardMedia
-                  style={{ height: 0, paddingTop: "25%" }}
-                  image={data.image}
-                  title={data.sick}
-                />
-                <CardContent>
-                  <Typography
-                    gutterBottom
-                    variant="h5"
-                    component="h2"
-                    color="primary"
-                  >
-                    {data.sick}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              <CardContent style={{ background: "#f9f9f9" }}>
-                <Typography component="p">
-                  <FoodList cardData={data} />
+const FoodCard = props => {
+  return (
+    <Grid container spacing={8}>
+      {props.FoodData.map((disease, index) => (
+        <Grid item xs={12} md={6} lg={3} style={{ display: "flex" }}>
+          <Card raised key={index} style={{ marginBottom: 15, width: "100%" }}>
+            <CardActionArea style={{ width: "100%" }}>
+              <CardMedia
+                style={{ height: 0, paddingTop: "25%" }}
+                image={disease.image}
+                title={disease.sick}
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="h2"
+                  color="primary"
+                >
+                  {disease.sick}
                 </Typography>
               </CardContent>
-              <CardActions>
+            </CardActionArea>
+            <CardContent style={{ background: "#f9f9f9" }}>
+              <Typography component="p">
+                <FoodList cardData={disease} />
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary">
+                Share
+              </Button>
+              <Link
+                as={`/foodDetails/${disease.searchKey}`}
+                href={{
+                  pathname: "/foodDetails",
+                  query: { disease: disease.searchKey }
+                }}
+              >
                 <Button size="small" color="primary">
-                  Share
+                  Learn More
                 </Button>
-                <Link
-                  as={`/foodDetails/${data.searchKey}`}
-                  href={{
-                    pathname: "/foodDetails",
-                    query: { disease: data.searchKey }
-                  }}
-                >
-                  <Button size="small" color="primary">
-                    Learn More
-                  </Button>
-                </Link>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    );
-  }
-}
+              </Link>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 
 export default FoodCard;
